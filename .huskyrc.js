259ae6b3e74e07a3d996826477081e90cd48b8af
kongxiangyan <kcigaret@outlook.com>
2019-08-16 22:14:08 +0800

    chore: update

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,8 @@
+const tasks = arr => arr.join(' && ')
 module.exports = {
     "hooks": {
         // unuseable under windows
         // "prepare-commit-msg": "exec < /dev/tty && npx git cz --hook || true",
+        "commit-msg": "commitlint -E $HUSKY_GIT_PARAMS"
     }
-    
 }
