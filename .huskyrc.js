733a7ae0fb5ece4b9edbb415af866fe39ce89ea3
kongxiangyan <kcigaret@outlook.com>
2019-08-17 20:04:32 +0800

    chore(update): update scaffolding

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@ module.exports = {
   hooks: {
     // unuseable under windows
     // "prepare-commit-msg": "exec < /dev/tty && npx git cz --hook || true",
-    'commit-msg': 'npx commitlint -E $HUSKY_GIT_PARAMS'
+    'commit-msg': 'npx commitlint -E HUSKY_GIT_PARAMS'
   }
 }
