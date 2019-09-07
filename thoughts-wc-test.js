acb3606abe2aa8f4cee0233c828985ced4df0b69
kongxiangyan <kcigaret@outlook.com>
2019-09-07 23:17:25 +0800

    chore: add semantic-release

--- thoughts-wc-test.js
+++ thoughts-wc-test.js
@@ -1,22 +1,23 @@
-import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js'
 class ThoughtsWcTest extends PolymerElement {
-    static get template() {
-        return html `
+  static get template() {
+    return html`
       <style>
         :host {
           display: block;
         }
       </style>
       <h2>Hello [[prop1]]!</h2>
-    `;
+    `
+  }
+
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'thoughts-wc-test'
+      }
     }
-    static get properties() {
-        return {
-            prop1: {
-                type: String,
-                value: "thoughts-wc-test"
-            }
-        };
-    }
+  }
 }
-window.customElements.define("thoughts-wc-test", ThoughtsWcTest);
+window.customElements.define('thoughts-wc-test', ThoughtsWcTest)
